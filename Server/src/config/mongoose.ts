ab28b6683aae88d292914ef5d24c66bb10eb939a
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2026-07-09 11:44:58 +0300

    DNS lookup updated

--- Server/src/config/mongoose.ts
+++ Server/src/config/mongoose.ts
@@ -1,12 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dns from "node:dns";
+import dotenv from "dotenv";
+dotenv.config();
+
+if (process.env.NODE_ENV === "development") {
+	dns.setServers(["8.8.8.8", "1.1.1.1"]);
+}
 
 const localDB = "mongodb://127.0.0.1:27017/job-seeker-db";
 
 export async function connectToDatabase() {
-    try {
-        await mongoose.connect(localDB);
-        console.log('Database is running...');
-    } catch (error) {
-       console.log("Error connecting to the database:", error);
-    }
+	try {
+		await mongoose.connect(process.env.DB_URI || localDB);
+		console.log("Database is running...");
+	} catch (error) {
+		console.log("Error connecting to the database:", error);
+	}
 }
