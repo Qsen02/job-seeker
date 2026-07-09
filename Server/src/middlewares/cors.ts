5130b49b65f48de59627a8d2de60b06b08654e70
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2026-07-09 12:05:00 +0300

    Cors updated

--- Server/src/middlewares/cors.ts
+++ Server/src/middlewares/cors.ts
@@ -2,7 +2,17 @@ import { NextFunction, Request, Response } from "express";
 
 export function setCors() {
 	return function (req: Request, res: Response, next: NextFunction) {
-		res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+		if (process.env.NODE_ENV === "development") {
+			res.setHeader(
+				"Access-Control-Allow-Origin",
+				"http://localhost:5173",
+			);
+		} else {
+			res.setHeader(
+				"Access-Control-Allow-Origin",
+				"https://job-seeker-gq5k.onrender.com",
+			);
+		}
 		res.setHeader(
 			"Access-Control-Allow-Methods",
 			"OPTIONS,GET,POST,DELETE,PUT",
