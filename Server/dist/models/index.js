8b419faf45ea136505efb557768c324efd3b4d09
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2026-07-09 12:02:07 +0300

    Update

--- Server/dist/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./users");
-require("./candidatures");
-require("./jobs");
-require("./companies");
--- Server/dist/utils/errorParser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseError = parseError;
-function parseError(errors) {
-    return errors
-        .array()
-        .map((el) => el.msg)
-        .join("\n");
-}
